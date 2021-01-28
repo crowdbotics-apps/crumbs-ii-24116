57477fb459402aa5478eda9263426b8430cd120e
Crowdbotics <team@crowdbotics.com>
2021-01-28 07:56:32 +0000

    Update components

--- src/features/BlankScreen2197592/screens/blank.js
+++ src/features/BlankScreen2197592/screens/blank.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { connect } from "react-redux"
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +11,58 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
-export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View>
+        <View style={styles.View_6}>
+          <View style={styles.View_11}>
+            <View />
+            <View />
+          </View>
+        </View>
+        <View style={styles.View_7}>
+          <Text>Welcome to Crumbs!</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  View_5: {},
+  View_6: {
+    width: "100%",
+    height: "100%",
+    alignSelf: "flex-start",
+    flexDirection: "row"
   },
-});
+  View_11: { flexDirection: "row" },
+  View_12: {},
+  View_13: {},
+  View_7: { width: "100%", height: "50%", alignSelf: "center" },
+  Text_17: {}
+})
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
